dc6a2edb033189a6fef4c29792905b05304be90e
Your Name <you@example.com>
2023-05-23 18:56:32 +0200

    proto(), source() and dest() functions for ipv6 packets

--- src/ip/v6/packet.rs
+++ src/ip/v6/packet.rs
@@ -13,8 +13,10 @@
 //  0. You just DO WHAT THE FUCK YOU WANT TO.
 
 use std::fmt;
+use std::net::Ipv6Addr;
 use crate::error::*;
 use crate::packet::{Packet as P, PacketMut as PM, AsPacket, AsPacketMut};
+use crate::ip::Protocol;
 
 /// IPv6 packet parser.
 #[derive(Clone)]
@@ -80,6 +82,41 @@ impl<B: AsRef<[u8]>> Packet<B> {
 	/// implements `Clone` and the impl would conflict.
 	pub fn to_owned(&self) -> Packet<Vec<u8>> {
 		Packet::unchecked(self.buffer.as_ref().to_vec())
+	}
+}
+
+impl<B: AsRef<[u8]>> Packet<B> {
+	/// Protocol of the inner packet.
+	pub fn protocol(&self) -> Protocol {
+		self.buffer.as_ref()[6].into()
+	}
+
+	/// Source IP address.
+	pub fn source(&self) -> Ipv6Addr {
+		Ipv6Addr::new(
+			((self.buffer.as_ref()[8]) as u16 ) << 8 | self.buffer.as_ref()[9] as u16,
+			((self.buffer.as_ref()[10]) as u16 ) << 8 | self.buffer.as_ref()[11] as u16,
+			((self.buffer.as_ref()[12]) as u16 ) << 8 | self.buffer.as_ref()[13] as u16,
+			((self.buffer.as_ref()[14]) as u16 ) << 8 | self.buffer.as_ref()[15] as u16,
+			((self.buffer.as_ref()[16]) as u16 ) << 8 | self.buffer.as_ref()[17] as u16,
+			((self.buffer.as_ref()[18]) as u16 ) << 8 | self.buffer.as_ref()[19] as u16,
+			((self.buffer.as_ref()[20]) as u16 ) << 8 | self.buffer.as_ref()[21] as u16,
+			((self.buffer.as_ref()[22]) as u16 ) << 8 | self.buffer.as_ref()[23] as u16,
+			)
+	}
+
+	/// Destination IP address.
+	pub fn destination(&self) -> Ipv6Addr {
+		Ipv6Addr::new(
+			((self.buffer.as_ref()[24]) as u16 ) << 8 | self.buffer.as_ref()[25] as u16,
+			((self.buffer.as_ref()[26]) as u16 ) << 8 | self.buffer.as_ref()[27] as u16,
+			((self.buffer.as_ref()[28]) as u16 ) << 8 | self.buffer.as_ref()[29] as u16,
+			((self.buffer.as_ref()[30]) as u16 ) << 8 | self.buffer.as_ref()[31] as u16,
+			((self.buffer.as_ref()[32]) as u16 ) << 8 | self.buffer.as_ref()[33] as u16,
+			((self.buffer.as_ref()[34]) as u16 ) << 8 | self.buffer.as_ref()[35] as u16,
+			((self.buffer.as_ref()[36]) as u16 ) << 8 | self.buffer.as_ref()[37] as u16,
+			((self.buffer.as_ref()[38]) as u16 ) << 8 | self.buffer.as_ref()[39] as u16,
+			)
 	}
 }
 
